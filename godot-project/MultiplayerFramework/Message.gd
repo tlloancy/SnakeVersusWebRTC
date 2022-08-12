be6b41d85bf45a5f0315f0c9082a2a54e4e2b211
tlloancy <tlloancy@student.42.fr>
2022-08-12 23:59:01 +0200

    Update Message.gd

--- godot-project/MultiplayerFramework/Message.gd
+++ godot-project/MultiplayerFramework/Message.gd
@@ -3,12 +3,16 @@ class_name Message
 const SERVER_LOGIN = 1
 const MATCH_START = 2
 const IS_ECHO = 4
+const DISCONNECTED_CLOSED = 8
+const DISCONNECTED_DISCONNECTED = 16
 
 const _BYTE_MASK = 255
 
 var server_login : bool
 var match_start : bool
 var is_echo : bool
+var disconnected_closed : bool
+var disconnected_disconnected : bool
 
 var content
 
@@ -19,6 +23,8 @@ func get_raw() -> PoolByteArray:
 	byte = set_bit(byte, SERVER_LOGIN, server_login)
 	byte = set_bit(byte, IS_ECHO, is_echo)
 	byte = set_bit(byte, MATCH_START, match_start)
+	byte = set_bit(byte, DISCONNECTED_CLOSED, disconnected_closed)
+	byte = set_bit(byte, DISCONNECTED_DISCONNECTED, disconnected_disconnected)
 	
 	message.append(byte)
 	message.append_array(var2bytes(content))
@@ -31,6 +37,8 @@ func from_raw(var arr : PoolByteArray):
 	server_login = get_bit(flags, SERVER_LOGIN)
 	is_echo = get_bit(flags, IS_ECHO)
 	match_start = get_bit(flags, MATCH_START)
+	disconnected_closed = get_bit(flags, DISCONNECTED_CLOSED)
+	disconnected_disconnected = get_bit(flags, DISCONNECTED_DISCONNECTED)
 	
 	content = null
 	if (arr.size() > 1):
