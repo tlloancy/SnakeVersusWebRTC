3db8d319376c1d02ff0ce6e5e9bf75ebcd1ceb26
tlloancy <thomaslloancy@gmail.com>
2023-11-01 00:09:15 +0100

    Update MatchmakerServer.gd

--- godot-project/MultiplayerFramework/MatchmakerServer.gd
+++ godot-project/MultiplayerFramework/MatchmakerServer.gd
@@ -115,7 +115,7 @@ func _process(delta):
 		var buf = 0
 		var state = _conn.get_status()
 		if state == StreamPeerTCP.STATUS_CONNECTED:
-			while buf = _conn.get_available_bytes():
+			while true = _conn.get_available_bytes():
 				_on_data(buf, _conn)
 		elif state == StreamPeerTCP.STATUS_CONNECTING:
 			pass
