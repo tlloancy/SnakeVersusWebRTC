0d5fa7c392a847c3c7ce971d361afeee96ee0b66
tlloancy <thomaslloancy@gmail.com>
2023-11-02 21:58:42 +0100

    Update MatchmakerServer.gd

--- godot-project/MultiplayerFramework/MatchmakerServer.gd
+++ godot-project/MultiplayerFramework/MatchmakerServer.gd
@@ -4,7 +4,8 @@ extends Node
 
 const PORT = 9080
 var _server = TCPServer.new()
-var spTCP
+var wsp : WebSocketPeer
+var spTCP : StreamPeerTCP
 var _connected_players = {}
 var _match_queue = []
 
@@ -43,12 +44,12 @@ func _connected(id, proto):
 	var message = Message.new()
 	message.server_login = true
 	message.content = id
-	id.put_data(message.get_raw())
+	id.put_packet(message.get_raw())
 
 func _match_size():
 	pass
 
-func create_new_match(spTCP):
+func create_new_match(wsp):
 	var new_match = []
 	for i in range(match_size):
 		new_match.append(_match_queue[i])
@@ -57,7 +58,7 @@ func create_new_match(spTCP):
 		var message = Message.new()
 		message.match_start = true
 		message.content = new_match
-		spTCP.put_data(message.get_raw())
+		wsp.put_packet(message.get_raw())
 		_match_queue.remove_at(0)
 
 	for i in range(new_match.size()):
@@ -81,7 +82,7 @@ func _close_request(id, code, reason):
 
 	for player_id in _connected_players[id]:
 		if (player_id != id):
-			id.put_data(message.get_raw())
+			id.put_packet(message.get_raw())
 
 	remove_player_from_connections(id)
 
@@ -94,22 +95,21 @@ func _disconnected(id, was_clean = false):
 
 	for player_id in _connected_players[id]:
 		if (player_id != id):
-			id.put_data(message.get_raw())
+			id.put_packet(message.get_raw())
 		
 	remove_player_from_connections(id)
 
-func _on_data(buf, id, ids):
+func _on_data(id):
 	var message = Message.new()
 	print("j ai rentré quand meme!")
-	print(buf)
 	var res = id.get_packet()
 	#var res = id.get_data(buf)
 	print(res)
 	message.from_raw(res)
 	print("un")
 	for player_id in _connected_players[ids]:
-		print(ids)
-		if (player_id != ids || (player_id == ids && message.is_echo)):
+		print(id)
+		if (player_id != id || (player_id == id && message.is_echo)):
 			print(player_id)
 			id.put_packet(message.get_raw())
 	print("quatro")
@@ -117,31 +117,28 @@ func _on_data(buf, id, ids):
 func _process(delta):
 	if _server.is_connection_available():
 		spTCP = _server.take_connection()
-		_connected(spTCP, "TCP")
+		wsp = WebSocketPeer.new()
+		wsp.accept_stream(spTCP)
+		_connected(wsp, "TCP")
 
 	for _conn in _connected_players.keys():
 		_conn.poll()
 		var buf = 0
-		var state = _conn.get_status()
-		if state == StreamPeerTCP.STATUS_CONNECTED:
-			var wsp = WebSocketPeer.new()
-			wsp.accept_stream(spTCP)
-			wsp.poll()
-			var status = wsp.get_ready_state()
-			if status == WebSocketPeer.STATE_OPEN:
-				while wsp.get_available_packet_count():
-					_on_data(buf, wsp, _conn)
+		var state = _conn.get_ready_state()
+		if state == WebSocketPeer.STATE_OPEN:
+			while wsp.get_available_packet_count():
+				_on_data(wsp)
 			#while true:
 			#	buf = _conn.get_available_bytes()
 			#	if buf <= 0:
 			#		break
 			#	_on_data(buf, _conn)
-		elif state == StreamPeerTCP.STATUS_CONNECTING:
+		elif state == WebSocketPeer.STATE_CONNECTING:
 			pass
-		elif state == StreamPeerTCP.STATUS_NONE:
+		elif state == WebSocketPeer.STATE_CLOSING:
 			_disconnected(_conn)
-		else: # STATUS_ERROR
-			_close_request(_conn, 00, "ERROR")
+		else: # STATUS_CLOSE
+			_close_request(_conn, 00, "CLOSE")
 			pass
 
 		if (_match_queue.size() >= match_size):
